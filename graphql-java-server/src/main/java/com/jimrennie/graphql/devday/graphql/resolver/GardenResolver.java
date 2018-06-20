d692493c858f13010878b0161e6bf0f7f5b044ce
Unknown <lex.pattison@fcc-fac.ca>
2018-06-20 09:36:54 -0600

    Batch call... not working yet

--- graphql-java-server/src/main/java/com/jimrennie/graphql/devday/graphql/resolver/GardenResolver.java
+++ graphql-java-server/src/main/java/com/jimrennie/graphql/devday/graphql/resolver/GardenResolver.java
@@ -8,13 +8,18 @@ import com.jimrennie.graphql.devday.graphql.api.PlantDto;
 import com.jimrennie.graphql.devday.graphql.api.ZombieDto;
 import com.jimrennie.graphql.devday.graphql.assembler.PlantDtoAssembler;
 import com.jimrennie.graphql.devday.graphql.assembler.ZombieDtoAssembler;
-import graphql.execution.batched.Batched;
+import lombok.Getter;
+import lombok.Setter;
+import lombok.experimental.Accessors;
 import org.dataloader.BatchLoader;
+import org.dataloader.DataLoader;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Component;
 
 import java.util.List;
 import java.util.Optional;
+import java.util.concurrent.CompletableFuture;
+import java.util.concurrent.CompletionStage;
 import java.util.stream.Collectors;
 
 @Component
@@ -39,11 +44,33 @@ public class GardenResolver implements GraphQLResolver<GardenDto> {
 	}
 
 	public List<ZombieDto> getZombies(GardenDto gardenDto, String zombieType) {
-		return Optional.ofNullable(zombieType)
-				.map(type -> zombieService.findZombiesByGardenIdAndZombieType(gardenDto.getId(), zombieType))
-				.orElseGet(() -> zombieService.findZombiesByGardenId(gardenDto.getId()))
-				.stream()
-				.map(zombieDtoAssembler::assemble)
-				.collect(Collectors.toList());
+
+		BatchLoader<GardenDtoType, ZombieDto> batchLoader = new BatchLoader<GardenDtoType, ZombieDto>() {
+			@Override
+			public CompletionStage<List<ZombieDto>> load(List<GardenDtoType> gardenDtoType) {
+				return CompletableFuture.supplyAsync(() -> {
+					return gardenDtoType.stream()
+							.flatMap(gdt -> {
+								return Optional.ofNullable(gdt.getType())
+										.map(type -> zombieService.findZombiesByGardenIdAndZombieType(gdt.garden.getId(), gdt.getType()))
+										.orElseGet(() -> zombieService.findZombiesByGardenId(gdt.garden.getId()))
+										.stream()
+										.map(zombieDtoAssembler::assemble);
+							})
+							.collect(Collectors.toList());
+				});
+			}
+		};
+		DataLoader<GardenDtoType, ZombieDto> zombieLoder = new DataLoader<GardenDtoType, ZombieDto>(batchLoader);
+		zombieLoder.load(new GardenDtoType().setGarden(gardenDto).setType(zombieType));
+		return zombieLoder.dispatchAndJoin();
+	}
+
+	@Getter
+	@Setter
+	@Accessors(chain = true)
+	class GardenDtoType {
+		GardenDto garden;
+		String type;
 	}
 }
