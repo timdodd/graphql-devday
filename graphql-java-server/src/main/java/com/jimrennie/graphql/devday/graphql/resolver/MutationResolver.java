2c7875566b8ff016607c91e22355a93b80a7d22a
Unknown <lex.pattison@fcc-fac.ca>
2018-06-20 20:40:42 -0600

    Injure Zombie!

--- graphql-java-server/src/main/java/com/jimrennie/graphql/devday/graphql/resolver/MutationResolver.java
+++ graphql-java-server/src/main/java/com/jimrennie/graphql/devday/graphql/resolver/MutationResolver.java
@@ -3,8 +3,11 @@ package com.jimrennie.graphql.devday.graphql.resolver;
 import com.coxautodev.graphql.tools.GraphQLMutationResolver;
 import com.jimrennie.graphql.devday.core.entity.Plant;
 import com.jimrennie.graphql.devday.core.service.PlantService;
+import com.jimrennie.graphql.devday.core.service.ZombieService;
 import com.jimrennie.graphql.devday.graphql.api.PlantDto;
+import com.jimrennie.graphql.devday.graphql.api.ZombieDto;
 import com.jimrennie.graphql.devday.graphql.assembler.PlantDtoAssembler;
+import com.jimrennie.graphql.devday.graphql.assembler.ZombieDtoAssembler;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Component;
 
@@ -15,8 +18,16 @@ public class MutationResolver implements GraphQLMutationResolver {
 	private PlantService plantService;
 	@Autowired
 	private PlantDtoAssembler plantDtoAssembler;
+	@Autowired
+	private ZombieService zombieService;
+	@Autowired
+	private ZombieDtoAssembler zombieDtoAssembler;
 
 	public PlantDto addPlant(String plantType, Integer quantity) {
 		return plantDtoAssembler.assemble(plantService.savePlant(new Plant().setPlantType(plantType).setQuantity(quantity)));
+	}
+
+	public ZombieDto plantHitZombie(Long zombieId, Integer hitPoints) {
+		return zombieDtoAssembler.assemble(zombieService.injureZombie(zombieId, hitPoints));
 	}
 }
