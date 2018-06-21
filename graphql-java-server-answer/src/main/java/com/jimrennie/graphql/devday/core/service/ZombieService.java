7f311a11d5432e240106046fb38f564e6bc75c34
Unknown <lex.pattison@fcc-fac.ca>
2018-06-21 09:10:55 -0600

    Fix hitZombie

--- graphql-java-server-answer/src/main/java/com/jimrennie/graphql/devday/core/service/ZombieService.java
+++ graphql-java-server-answer/src/main/java/com/jimrennie/graphql/devday/core/service/ZombieService.java
@@ -8,9 +8,11 @@ import lombok.extern.slf4j.Slf4j;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
+import javax.transaction.Transactional;
 import java.util.List;
 import java.util.Optional;
 
+@Transactional
 @Slf4j
 @Service
 public class ZombieService {
